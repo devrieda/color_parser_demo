ea41f6fe886cb41e8d94d19baafb4e63cda81df6
Derek DeVries <devrieda@gmail.com>
2013-05-15 09:41:14 -0600

    clean up method to grab colors from page

--- app.rb
+++ app.rb
@@ -10,16 +10,15 @@ end
 
 post '/parse' do 
   content_type :json  
-
-  uri = URI.parse(params[:url])
-
-  @colors = if uri.kind_of? URI::HTTP
-    parser  = ColorParser::Page.new(uri.to_s)
-    parser.colors.keys    
-  else
-    []
-  end
-
-  {:colors => @colors, :url => params[:url]}.to_json
+  {:colors => colors(params[:url]), :url => params[:url]}.to_json
 end
 
+def colors(url)
+  uri = URI.parse(url)
+  if uri.kind_of? URI::HTTP
+    parser = ColorParser::Page.new(uri.to_s)
+    parser.colors_by_frequency
+  else
+    []
+  end  
+end
